6cd78600bb6ec7d191cb7eb9d5c4b3b009108ed1
sam <Sam Pointer>
2017-03-05 11:38:55 +0000

    package into a .deb

---
+++ scripts/package.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if [[ ${CIRCLE_BUILD_NUM} ]]; then
+  iteration=${CIRCLE_BUILD_NUM}
+else
+  iteration=0
+fi
+
+for package_type in deb ; do
+  fpm -t ${package_type} \
+    -s dir \
+    --name kapo \
+    --version $(./kapo --version | awk '{print $3}') \
+    --iteration ${iteration} \
+    --license gplv3 \
+    --vendor 'Sam Pointer' \
+    --provides kapo \
+    --architecture $(uname -m) \
+    --maintainer sam@outsidethe.net \
+    --description "Wrap any command in a status socket." \
+    --url "https://github.com/sampointer/kapo" \
+    --prefix /usr/local/bin \
+    kapo
+done
