65851011a2c7c4d04da97b7d7145d6d3bacc3773
Sam Pointer <sam@outsidethe.net>
2020-04-12 20:42:07 +0100

    dynamic version number from build

--- version.go
+++ version.go
@@ -1,7 +1,9 @@
 package main
 
 //Name is the name of the software
-const Name string = "kapo"
+const name string = "kapo"
 
-//Version is the version of the software
-const Version string = "0.6.0"
+//Version is the version of the software. It should be passed in at build time
+//with '-ldflags "-X main.version=$VERSION"'. Commonly this is the release tag
+//of the form 1.2.3 or, for development builds, the current git SHA.
+var version string
